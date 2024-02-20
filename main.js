8cd8d3d1ea77d5b1e8d47b110c4e70ba0d71a5af
João Victor <joaovba2002@alu.ufc.br>
2024-02-20 10:23:29 -0300

    aula-06

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            message: 'Venda de meias!',
-            imgPath: './assets/image2.jpg',
-            color: 'Azul',
-            quantity: 2,
+            fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
         }
     }
 });
