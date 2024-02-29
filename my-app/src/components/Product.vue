cd3257c0245fe00a7771929449c84b43b3320850
João Victor <joaovba2002@alu.ufc.br>
2024-02-28 22:49:59 -0300

    aula-16

---
+++ my-app/src/components/Product.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+    product: Object
+});
+</script>
+
+<template>
+    <div class="product">
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p>{{ product.price }}</p>
+    </div>
+</template>
+
+<style>
+.product {
+    border: 1px solid #ccc;
+    padding: 1rem;
+    margin: 1rem 0;
+}
+
+.product h2 {
+    margin: 0;
+}
+
+.product p {
+    margin: 0;
+}
+</style>
--- my-app/src/views/HomeView.vue
+++ my-app/src/views/HomeView.vue
@@ -1,10 +1,58 @@
 <script setup>
+import Navbar from '@/components/Navbar.vue';
+import Product from '@/components/Product.vue';
+
 let msg = 'Vamos aprender Vue 3'
+
+const products = [
+  {
+    id: 1,
+    name: 'Produto 1',
+    description: 'Descrição do produto 1',
+    price: 9.99
+  },
+  {
+    id: 2,
+    name: 'Produto 2',
+    description: 'Descrição do produto 2',
+    price: 19.99
+  },
+  {
+    id: 3,
+    name: 'Produto 3',
+    description: 'Descrição do produto 3',
+    price: 29.99
+  },
+  {
+    id: 4,
+    name: 'Produto 4',
+    description: 'Descrição do produto 4',
+    price: 39.99
+  }
+]
+
 </script>
 
 <template>
-  <main>
-    <h1>Olá mundo</h1>
-    <p>{{ msg }}</p>
-  </main>
+  <div>
+    <header>
+      <Navbar page="home" />
+    </header>
+    <main>
+      <Product v-for="(product, index) in products" :key="index" :product="product" />
+    </main>
+  </div>
 </template>
+
+<style>
+main {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  max-width: 800px;
+  margin: auto;
+  justify-items: center;
+  align-items: center;
+  margin-top: 50px;
+  gap: 20px;
+}
+</style>
